f3ce59d493e9c2cbcbd1fbde4ca33a435281ef0a
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-12-07 02:33:51 -0500

    Add Morgan.

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-const express = require('express');
+const express = require('express'),
+	morgan = require('morgan');
+
 const app = express();
 
-let myLogger = (req, res, next) => {
-	console.log(req.url);
-	next();
-};
-
-let requestTime = (req, res, next) => {
-	req.requestTime = Date.now();
-	next();
-};
-
-app.use(myLogger);
-app.use(requestTime);
+app.use(morgan('common'));
 
 app.get('/', (req, res) => {
-	let responseText = 'Welcome to my app!';
-	responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-	res.send(responseText);
+	res.send('Welcome to my app!');
 });
 
 app.get('/secreturl', (req, res) => {
-	let responseText = 'This is a secret url with super top-secret content.';
-	responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-	res.send(responseText);
+	res.send('This is a secret url with super top-secret content.');
 });
 
 app.listen(8080, () => {
