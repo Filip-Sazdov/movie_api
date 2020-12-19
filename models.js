211ae1f0d4381c42d5364ccede27b6605de3221e
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-12-18 19:27:50 -0500

    Enable model export.

--- models.js
+++ models.js
@@ -27,4 +27,5 @@ let userSchema = mongoose.Schema({
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
 
-
+module.exports.Movie = Movie;
+module.exports.User = User;
