c2d36a0e9dd18c87e0df157d069f86e61ca4814c
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-12-24 03:08:02 -0500

    Add connection URI.

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ app.use(morgan('common'));
 // Express.static returns all the static files located within the folder name provided as an argument
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/movie_APIDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/movie_APIDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
 	res.send('Welcome to my Movie App!');
