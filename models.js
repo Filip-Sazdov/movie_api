fe500ea70a89f0b1cccc5ad48c3ac475ebfcf517
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-12-23 23:18:46 -0500

    Correct validatePassword.

--- models.js
+++ models.js
@@ -29,9 +29,9 @@ userSchema.statics.hashPassword = (password) => {
 	return bcrypt.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = (function (password) {
+userSchema.methods.validatePassword = function (password) {
 	return bcrypt.compareSync(password, this.Password);
-})();
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
