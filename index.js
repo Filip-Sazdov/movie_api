da572e7c409a6ba902706230ea3d38284205d463
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-12-27 23:40:35 -0500

    Add remove favorite and remove user validation.

--- index.js
+++ index.js
@@ -158,9 +158,10 @@ app.put(
 	'/users/:Username',
 	passport.authenticate('jwt', { session: false }),
 	[
-		check('Username', 'Username is required').isLength({ min: 5 }),
+		check('Username', 'Username must be at least 5 characters.').isLength({ min: 5 }),
 		check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
 		check('Password', 'Password is required').not().isEmpty(),
+		check('Password', 'Password must be at least 5 characters.').isLength({ min: 5 }),
 		check('Email', 'Email does not appear to be valid').isEmail(),
 	],
 	(req, res) => {
@@ -225,39 +226,57 @@ app.post(
 );
 
 //remove movie from favorites
-app.delete('/users/:Username/Movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
-	Users.findOneAndUpdate(
-		{ Username: req.params.Username },
-		{
-			$pull: { FavoriteMovies: req.params.MovieID },
-		},
-		{ new: true }, // This line makes sure that the updated document is returned
-		(err, updatedUser) => {
-			if (err) {
-				console.error(err);
-				res.status(500).send('Error: ' + err);
-			} else {
-				res.json(updatedUser);
+app.delete(
+	'/users/:Username/Movies/:MovieID',
+	passport.authenticate('jwt', { session: false }),
+	[
+		check('Username', 'Username is required').isLength({ min: 5 }),
+		check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+		check('MovieID', 'MovieID is required').not().isEmpty(),
+		check('MovieID', 'MovieID contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+	],
+	(req, res) => {
+		Users.findOneAndUpdate(
+			{ Username: req.params.Username },
+			{
+				$pull: { FavoriteMovies: req.params.MovieID },
+			},
+			{ new: true }, // This line makes sure that the updated document is returned
+			(err, updatedUser) => {
+				if (err) {
+					console.error(err);
+					res.status(500).send('Error: ' + err);
+				} else {
+					res.json(updatedUser);
+				}
 			}
-		}
-	);
-});
+		);
+	}
+);
 
 // Delete a user by username
-app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
-	Users.findOneAndRemove({ Username: req.params.Username })
-		.then((user) => {
-			if (!user) {
-				res.status(400).send(req.params.Username + ' was not found');
-			} else {
-				res.status(200).send(req.params.Username + ' was deleted.');
-			}
-		})
-		.catch((err) => {
-			console.error(err);
-			res.status(500).send('Error: ' + err);
-		});
-});
+app.delete(
+	'/users/:Username',
+	passport.authenticate('jwt', { session: false }),
+	[
+		check('Username', 'Username is required').isLength({ min: 5 }),
+		check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+	],
+	(req, res) => {
+		Users.findOneAndRemove({ Username: req.params.Username })
+			.then((user) => {
+				if (!user) {
+					res.status(400).send(req.params.Username + ' was not found');
+				} else {
+					res.status(200).send(req.params.Username + ' was deleted.');
+				}
+			})
+			.catch((err) => {
+				console.error(err);
+				res.status(500).send('Error: ' + err);
+			});
+	}
+);
 
 app.use((err, req, res, next) => {
 	console.error(err.stack);
